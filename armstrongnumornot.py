498a6c8b48e32fab42cdd450d08f60243f0fdfcc
Mekala-Mokshita <mokshita0106@gmail.com>
2025-09-15 19:31:23 +0530

    Add files via upload

---
+++ armstrongnumornot.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    cube = digit ** 3
+    sum = sum + cube
+    temp= temp // 10  
+
+if sum == num:
+    print("It is an Armstrong number")
+else:
+    print("It is not an Armstrong number")
