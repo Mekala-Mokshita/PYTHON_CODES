d16eac6d17dc11b5efa6af607867b233eade1c79
Mekala Mokshita <mokshita0106@gmail.com>
2025-12-02 22:00:00 +0530

    file updated

--- addlistnums.py
+++ addlistnums.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 nums = list(map(int, input("Enter numbers: ").split()))
 nn = []
 total = 0
@@ -6,12 +5,4 @@ for n in nums:
     total += n
     nn.append(total)
 print(nn)
-=======
-nums = list(map(int, input("Enter numbers: ").split()))
-nn = []
-total = 0
-for n in nums:
-    total += n
-    nn.append(total)
-print(nn)
->>>>>>> 43005180bed9657c65125617087e9739afc3082c
+
--- armstrongnumornot.py
+++ armstrongnumornot.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 num = int(input("Enter a number: "))
 temp = num
 sum = 0
@@ -13,19 +12,4 @@ if sum == num:
     print("It is an Armstrong number")
 else:
     print("It is not an Armstrong number")
-=======
-num = int(input("Enter a number: "))
-temp = num
-sum = 0
 
-while temp > 0:
-    digit = temp % 10
-    cube = digit ** 3
-    sum = sum + cube
-    temp= temp // 10  
-
-if sum == num:
-    print("It is an Armstrong number")
-else:
-    print("It is not an Armstrong number")
->>>>>>> 43005180bed9657c65125617087e9739afc3082c
