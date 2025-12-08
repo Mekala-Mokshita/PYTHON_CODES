55cdc03c773b2b53a3948eb498dbdee550b5430f
Mekala Mokshita <mokshita0106@gmail.com>
2025-12-08 22:00:00 +0530

    file updated

--- triangularpyramid.py
+++ triangularpyramid.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 rows = int(input("Enter number of rows: "))
 
 for i in range(1, rows + 1):
-=======
-rows = int(input("Enter number of rows: "))
-
-for i in range(1, rows + 1):
->>>>>>> fed5002755c7ca625ba412dd646b3f3962b95280
     print(" " * (rows - i) + "*" * (2 * i - 1))
