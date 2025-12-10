6288888c75c8ca92a728870121901de48acb0466
Mekala Mokshita <mokshita0106@gmail.com>
2025-12-10 22:00:00 +0530

    file updated

--- stroccur.py
+++ stroccur.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-#count occurrences of each character in a string:-
-word=input()
-char_count={}
-for char in word:
-    if char in char_count:
-        char_count[char]+=1
-    else:
-        char_count[char]=1
-for char,count in char_count.items():
 #count occurrences of each character in a string:-
 word=input()
 char_count={}
