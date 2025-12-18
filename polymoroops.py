0e84ab4fcc2f5d0cab84935d120044ddc30f347e
Mekala Mokshita <mokshita0106@gmail.com>
2025-12-18 22:00:00 +0530

    file added

--- polymoroops.py
+++ polymoroops.py
@@ -10,20 +10,6 @@ class Student:
 student1=Student("manohar",11,93)
 student2=Student("arjun",12,99)
 
-#child class
-class GraduateStudent(Student): 
-    def __init__(self,name,grade,percentage,stream):
-        super().__init__(name,grade,percentage) 
-        self.stream=stream
-        
-    def student_details(self):
-        print(f'{self.name} is in class {self.grade},with {self.percentage}% and from stream {self.stream}')
 
-#object-Student class
-student1=Student("manohar",11,93)
- #object-GraduateStudent class     
-Grad_Student1=GraduateStudent('Kingkonk',12,95,'PCM')
-student1.student_details()
-Grad_Student1.student_details()
 
         
