663be88b8ad512dc1365f10d887ba036b1e307d8
Mekala Mokshita <mokshita0106@gmail.com>
2025-12-25 22:00:00 +0530

    file updated

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 n=int(input())
 result = []
 for i in range(1, n + 1):
@@ -10,17 +9,3 @@ for i in range(1, n + 1):
         result.append("hub")
     else:
         result.append(str(i))
-=======
-n=int(input())
-result = []
-for i in range(1, n + 1):
-    if i % 3 == 0 and i % 5 == 0:
-        result.append("github")
-    elif i % 3 == 0:
-        result.append("git")
-    elif i % 5 == 0:
-        result.append("hub")
-    else:
-        result.append(str(i))
->>>>>>> 43005180bed9657c65125617087e9739afc3082c
-print(result)
