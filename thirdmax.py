e3373cd1f1b968bca5e257f78a39d5bc7eef60ea
Mekala Mokshita <mokshita0106@gmail.com>
2025-12-26 22:00:00 +0530

    file updated

--- thirdmax.py
+++ thirdmax.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #3rd max number:-
 nums=list(map(int, input("Enter numbers: ").split()))
 nums=set(nums) #set do not accept indexing
@@ -7,14 +6,4 @@ if len(nums)<3:
 else:
     nums.remove(max(nums))
     nums.remove(max(nums))
-=======
-#3rd max number:-
-nums=list(map(int, input("Enter numbers: ").split()))
-nums=set(nums) #set do not accept indexing
-if len(nums)<3:
-    print (max(nums))
-else:
-    nums.remove(max(nums))
-    nums.remove(max(nums))
->>>>>>> 43005180bed9657c65125617087e9739afc3082c
     print(max(nums))
