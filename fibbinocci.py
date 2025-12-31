78537c553061e13b5effaf645b74302323fdf213
Mekala Mokshita <mokshita0106@gmail.com>
2025-12-31 22:00:00 +0530

    file updated

--- fibbinocci.py
+++ fibbinocci.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-n=int(input())
+n=int(input("Enter the number of terms: "))
 a,b=0,1
 for i in range(n):
     print(a,end=" ")
-=======
-n=int(input())
-a,b=0,1
-for i in range(n):
-    print(a,end=" ")
->>>>>>> 43005180bed9657c65125617087e9739afc3082c
     a,b=b,a+b
