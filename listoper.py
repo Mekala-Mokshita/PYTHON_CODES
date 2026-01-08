b8bd8c44f2661e4673418380cb1737f0286f0e15
Mekala Mokshita <mokshita0106@gmail.com>
2026-01-08 22:00:00 +0530

    file updated

---
+++ listoper.py
@@ -0,0 +1,14 @@
+list=["apple","banana","a","b",1,2] 
+print(list)
+list.append(3)
+print(list)
+list.extend(["mango","grapes"])
+print(list)
+list.insert(1,4)
+print(list)
+list.remove(1)
+print(list)
+del list[1]
+print(list)
+list.pop(2)
+print(list)
