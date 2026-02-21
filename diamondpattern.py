7e4d0a81e7e6328df6148b6303c663b6847dc902
Mekala Mokshita <mokshita0106@gmail.com>
2026-02-21 22:00:00 +0530

    file added

--- diamondpattern.py
+++ diamondpattern.py
@@ -3,3 +3,5 @@ for i in range(1,n+1): #we will this loop in left side pyramid
     print(" "*(n-i),"* "*i)
 for i in range(n-1,0,-1): #if we use n,0,-1 then in the middle we will get same no of stars in two lines
     print(" "*(n-i),"* "*i)
+
+#we should two loops due to up and down 
